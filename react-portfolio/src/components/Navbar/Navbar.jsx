08e9b1eb4e2abc27ca9acdd7a60388b845daa761
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2021-04-07 20:03:15 -0400

    finished navbar

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,81 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
+import '../Navbar/Navbar.css';
 
-class Navbar extends Component {
-  render() {
+function Navbar(props) {
+
+    const onMouseHover = (obj) => {
+
+    }
+//   render() {
     return (
       <>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-              Navbar
-            </a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavDropdown"
-              aria-controls="navbarNavDropdown"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    Features
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    Pricing
-                  </a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a
-                    class="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarDropdownMenuLink"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown link
-                  </a>
-                  <ul
-                    class="dropdown-menu"
-                    aria-labelledby="navbarDropdownMenuLink"
-                  >
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
+        <div class="navbar-container centered">
+          <div class="ml-2 navbar-item-container centered">
+            <span class="font-weight-bold navbar-item centered">About Me</span>
+            <div class="navbar-item-selected navbar-border navbar-item"></div>
           </div>
-        </nav>
+          <div class="ml-2 navbar-item-container centered">
+            <span class="navbar-item centered">Projects</span>
+            <div class="underline-from-left navbar-border navbar-item"></div>
+          </div>
+          <div class="ml-2 navbar-item-container centered">
+            <span class="navbar-item centered">Resume</span>
+            <div class="underline-from-left navbar-border navbar-item"></div>
+          </div>
+          <div class="ml-2 navbar-item-container centered">
+            <span class="navbar-item centered">Contact Me</span>
+            <div class="underline-from-left navbar-border navbar-item"></div>
+          </div>
+        </div>
       </>
     );
   }
-}
 
 export default Navbar;
