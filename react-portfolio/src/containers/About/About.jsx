9ff1a9d819013dd80b49c8f924031de8af66e521
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2021-04-07 21:44:23 -0400

    resolving image

--- react-portfolio/src/containers/About/About.jsx
+++ react-portfolio/src/containers/About/About.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import profile from '../../images/Linkedin_profile';
 
 function About(props) {
     return (
         <div>
-            <img className="image" src='/public/Linkedin_profile.jpg' alt="profile picture"></img>
+            {/* <img className="image" src='https://media2.giphy.com/media/yYCrCYT2YxTDZ7RaGC/giphy.gif?cid=ecf05e471d78d965e42e7c7a0ed32ebc19b16c5ab6206603&rid=giphy.gif&ct=g' alt="profile picture"></img> */}
+            <img className="image" src={profile} alt="profile picture"></img>
+
             <p>Hi! I am Jonathan Garcia!</p>
 
             <p>I am currently a full stack engineer at ADP.</p>
@@ -12,4 +15,4 @@ function About(props) {
     );
 }
 
-export default About;
+export default About
