19d9f9f961f57e1c56d0055dfbf2ae3684767f9d
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2021-04-19 15:15:44 -0400

    ADP project

--- react-portfolio/src/containers/Projects/Projects.jsx
+++ react-portfolio/src/containers/Projects/Projects.jsx
@@ -7,7 +7,7 @@ function Projects(props) {
             <Project
             image={'https://media-exp1.licdn.com/dms/image/C4E0BAQGpM5b5Dk42aQ/company-logo_200_200/0/1552054428528?e=2159024400&v=beta&t=8LF1c47t7gbHHKaaIWbL2Kes7kYN-fgR0BI2J4FI7to'}            
             title="ADP"
-            description="As a member of the Transportation team, part of the Supply Chain division at The Home Depot Marietta Technology Center, I built and maintained numerous internal applications. Our biggest 'built-from-scratch' application, VendorPlanEx was responsible for cutting transportation costs on bulk items by nearly 30%, a savings of millions of dollars per year."
+            description="At ADP, I work on the Login and Contextual Home Page Team as a Full Stack Engineer."
             technologies={[
               "React",
               "Express",
